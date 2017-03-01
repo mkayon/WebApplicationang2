74732ec8278246c8368cbae99668986afa345182
mkayon <mkayon@yandex.ua>
2017-03-01 22:16:13 +0200

    Add project files.

---
+++ WebApplicationang2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationang2.Startup))]
+namespace WebApplicationang2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
